9533a977b6754389db068ab4f83be5d433c2b57a
tawhid0406@gmail.com <tawhid0406@gmail.com>
2024-12-03 20:47:11 +0600

    update

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,19 +1,19 @@
 export const Bio = {
-  name: "Rishav Chanda",
+  name: "Md Tawhidul Islam",
   roles: [
-    "Full Stack Developer",
-    "Android Developer",
-    "UI/UX Designer",
+    "Web Designer",
+    "Web Developer",
+    "App Designer",
     "Programmer",
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-  github: "https://github.com/rishavchanda",
+  github: "https://github.com/fmtawhid",
   resume:
     "https://drive.google.com/file/d/1txFKZD3LNIXMc329jD9c995AU8LItOXK/view?usp=drive_link",
-  linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+  linkedin: "https://www.linkedin.com/in/fmtawhid/",
   twitter: "https://twitter.com/RishavChanda",
-  insta: "https://www.instagram.com/rishav_chanda/",
+  insta: "https://www.instagram.com/fmtawhid/",
   facebook: "https://www.facebook.com/rishav.chanda.165/",
 };
 
